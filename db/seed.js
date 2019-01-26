d26cce1a41f7b88018c94f8af520bcc6e5fb80f7
Stuart Fooks <sb.fooks@gmail.com>
2019-01-26 11:26:49 -0500

    populate thumbnails

--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,63 @@
 const Thumbnail = require("../models/Thumbnail")
+const source = "/images/thumbnails/thumbnail-"
 
 Thumbnail.create({
     title: "A Simple Favor",
-    source: "/images/thumbnails/thumbnail-a-simple-favor.jpg",
+    source: source + "a-simple-favor.jpg",
+    year: 2018
+},
+{
+    title: "John Adams",
+    source: source + "abigail-john-adams.jpg",
+    year: 2018
+}, 
+{
+    title: "Battle of the Sexes",
+    source: source + "battle-of-the-sexes.jpg",
+    year: 2018
+},
+{
+    title: "Come Sunday",
+    source: source + "come-sunday.jpg",
+    year: 2018
+}, 
+{
+    title: "Feud",
+    source: source + "feud.jpg",
+    year: 2018
+},
+{
+    title: "Ghost Busters",
+    source: source + "ghost-busters.jpg",
+    year: 2018
+},
+{
+    title: "John Adams",
+    source: source + "john-adams-franklin.jpg",
+    year: 2018
+},
+{
+    title: "John Adams",
+    source: source + "john-adams-jefferson.jpg",
+    year: 2018
+},
+{
+    title: "John Adams",
+    source: source + "john-adams.jpg",
+    year: 2018
+},
+{
+    title: "Mercy Street",
+    source: source + "mercy-street.jpg",
+    year: 2018
+},
+{
+    title: "Montage",
+    source: source + "montage.jpg",
+    year: 2018
+},
+{
+    title: "Why Him",
+    source: source + "why-him.jpg",
     year: 2018
 })
